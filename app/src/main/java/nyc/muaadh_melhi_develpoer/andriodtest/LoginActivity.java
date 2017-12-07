2679a7c650f55032fe50971bf7dfea24f41efd43
Muaadh Melhi <Muaadhmelhi@ac.c4q.nyc>
2017-12-06 20:32:53 -0500

    added part02

--- app/src/main/java/nyc/muaadh_melhi_develpoer/andriodtest/LoginActivity.java
+++ app/src/main/java/nyc/muaadh_melhi_develpoer/andriodtest/LoginActivity.java
@@ -1,13 +1,67 @@
 package nyc.muaadh_melhi_develpoer.andriodtest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String SHARED_PREFS_KEY = "sharedPrefsTesting";
+    private static final String EMAIL_KEY = "email";
+    private EditText username;
+    private EditText password;
+    private CheckBox checkBox;
+    private Button submitButton;
+    private SharedPreferences login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = (EditText) findViewById(R.id.username_edittext);
+        password = (EditText) findViewById(R.id.password_edittext);
+        checkBox = (CheckBox) findViewById(R.id.remember_me_checkbox);
+        submitButton = (Button) findViewById(R.id.submit_button);
+
+
+        login = getApplicationContext().getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE);
+
+        //get the data when we came back
+        if (login.getBoolean("isChecked", false)) {
+            username.setText(login.getString("username", null));
+            password.setText(login.getString("password", null));
+            checkBox.setChecked(login.getBoolean("isChecked", false));
+        }
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences.Editor editor = login.edit();
+                if (checkBox.isChecked()) {
+                    editor.putString("username", username.getText().toString());
+                    editor.putString("password", password.getText().toString());
+                    editor.putBoolean("isChecked", checkBox.isChecked());
+                    editor.commit();
+                } else {
+                    editor.putBoolean("isChecked", checkBox.isChecked());
+                    editor.commit();
+                }
+
+                if (username.equals("user@aol.com") && password.equals("password1234")) {
+                    Intent intent = new Intent(LoginActivity.this, ListActivity.class);
+                    intent.putExtra(EMAIL_KEY, username.getText().toString());
+                    startActivity(intent);
+
+                }
+            }
+
+        });
+
     }
 }
+
